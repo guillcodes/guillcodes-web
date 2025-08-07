677ecfa81ab925172bdabc52af6b511b78130ef4
guillcodes <guilcodez@gmail.com>
2025-08-07 20:31:23 -0300

    Criando a estrutura base para o desenvolvimento do projeto

---
+++ Portifolio/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Seu código aqui
+
+    function init() {
+        console.log('Script carregado e pronto!');
+        // Inicialize outras funções aqui
+    }
+
+    init();
+});
